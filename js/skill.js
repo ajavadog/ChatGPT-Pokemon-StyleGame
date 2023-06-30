577fa6e2259b33a187e02da720ab961835a6a4d7
ajavadog <supercellqq88@gmail.com>
2023-06-30 15:29:14 +0800

    Update skill.js

--- js/skill.js
+++ js/skill.js
@@ -9,18 +9,18 @@ class Skill {
     this.spriteImage.src = spriteImagePath;
   }
 
-  getTypeMultiplier(attackerType, defenderType) {
-    if (attackerType === 'fire' && defenderType === 'grass') {
+  getTypeMultiplier(attackerType, defenderType) {//   高冷<->瑟瑟  ->可愛->帥氣->抖M->傲嬌
+    if (attackerType === '高冷' && defenderType === '瑟瑟') {
       return 2;
-    } else if (attackerType === 'grass' && defenderType === 'water') {
+    } else if (attackerType === '可愛' && defenderType === '帥氣') {
       return 2;
-    } else if (attackerType === 'water' && defenderType === 'fire') {
+    } else if (attackerType === '帥氣' && defenderType === '抖M') {
       return 2;
-    } else if (attackerType === 'ground' && defenderType === 'electric') {
+    } else if (attackerType === '抖M' && defenderType === '傲嬌') {
       return 2;
-    } else if (attackerType === 'electric' && defenderType === 'water') {
+    } else if (attackerType === '傲嬌' && defenderType === '可愛') {
       return 2;
-    } else if (attackerType === 'water' && defenderType === 'ground') {
+    } else if (attackerType === '瑟瑟' && defenderType === '高冷') {
       return 2;
     } else {
       return 1;
@@ -47,5 +47,66 @@ class Skill {
     }
     ctx.drawImage(this.spriteImage, x, y, 40, 40);
     ctx.restore();
+  }
+}
+
+// Example Skill - Heal
+class Heal extends Skill {
+  constructor(name, healAmount, pp, type, effectType, spriteImagePath) {
+    super(name, 0, pp, type, effectType, spriteImagePath);
+    this.healAmount = healAmount;
+  }
+
+  use(user,effectType) {
+    user.currentHealth += this.healAmount;
+    if (user.currentHealth > user.maxHealth) {
+      user.currentHealth = user.maxHealth;
+    }
+    this.pp--;
+    return `${user.name}使用了${this.name}，恢復了${this.healAmount}點生命值！`;
+  }
+}
+
+// Example Skill - Paralyze
+class Paralyze extends Skill {
+  constructor(name, pp, type, effectType, spriteImagePath) {
+    super(name, 0, pp, type, effectType, spriteImagePath);
+  }
+
+  use(user, target) {
+    target.isParalyzed = true;
+    this.pp--;
+    return `${user.name}使用了${this.name}，${target.name}陷入麻痺狀態！`;
+  }
+}
+
+// Example Skill - Weaken
+class Weaken extends Skill {
+  constructor(name, weakenAmount, pp, type, effectType, spriteImagePath) {
+    super(name, 0, pp, type, effectType, spriteImagePath);
+    this.weakenAmount = weakenAmount;
+  }
+
+  use(user, target) {
+    target.damage -= this.weakenAmount;
+    if (target.damage < 0) {
+      target.damage = 0;
+    }
+    this.pp--;
+    return `${user.name}使用了${this.name}，${target.name}攻擊力下降了${this.weakenAmount}點！`;
+  }
+}
+
+// Example Skill - Shield
+class Shield extends Skill {
+  constructor(name, shieldAmount, pp, type, effectType, spriteImagePath) {
+    super(name, 0, pp, type, effectType, spriteImagePath);
+    this.shieldAmount = shieldAmount;
+  }
+
+  use(user, target) {
+    user.shield += this.shieldAmount;
+    this.pp--;
+    return `${user.name}使用了${this.name}，獲得了${this.shieldAmount}點盾牌值！`;
   }
 }
